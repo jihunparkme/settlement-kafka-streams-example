68f414015664c8b958ddf265e8feabfcce7b8936
Jihun Park <jihunpark.tech@gmail.com>
2025-06-23 21:51:11 +0900

    fix: TestContainerConfig

--- src/test/kotlin/com/example/settlement/kafka/streams/application/SettlementKafkaStreamsAppTest.kt
+++ src/test/kotlin/com/example/settlement/kafka/streams/application/SettlementKafkaStreamsAppTest.kt
@@ -5,8 +5,10 @@ import com.example.settlement.kafka.streams.application.client.PayoutRuleClient
 import com.example.settlement.kafka.streams.application.config.KafkaProperties
 import com.example.settlement.kafka.streams.application.domain.rule.Rule
 import com.example.settlement.kafka.streams.application.service.SettlementService
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.test.context.ActiveProfiles
@@ -16,15 +18,15 @@ import org.springframework.test.context.ContextConfiguration
 @ActiveProfiles("test")
 @ExtendWith(TestContainerConfig::class)
 @ContextConfiguration(initializers = [IntegrationTestInitializer::class])
-class SettlementKafkaStreamsAppTest(
-    private val kafkaProperties: KafkaProperties,
-    private val serdeFactory: SerdeFactory,
-    private val settlementService: SettlementService,
-    private val payoutRuleClient: PayoutRuleClient,
-    private val ruleKafkaTemplate: KafkaTemplate<String, Rule>,
-) {
+class SettlementKafkaStreamsAppTest {
+    @Autowired private lateinit var kafkaProperties: KafkaProperties
+    @Autowired private lateinit var serdeFactory: SerdeFactory
+    @Autowired private lateinit var settlementService: SettlementService
+    @Autowired private lateinit var payoutRuleClient: PayoutRuleClient
+    @Autowired private lateinit var ruleKafkaTemplate: KafkaTemplate<String, Rule>
+
     @Test
     fun test() {
-        println("SUCCESS")
+        Assertions.assertEquals("SUCCESS", "SUCCESS")
     }
 }
